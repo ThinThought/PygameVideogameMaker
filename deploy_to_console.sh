6d97029f5b6c0c601c0ece2224a1e0699f4728a3
Diego Delgado Chaves <diedelcha@gmail.com>
2026-01-03 13:45:41 +0100

    translations. Quality of life

--- deploy_to_console.sh
+++ deploy_to_console.sh
@@ -1,38 +1,31 @@
 #!/usr/bin/env sh
-
-IP="$DEPLOY_CONSOLE_IP"
-
-if [ -z $IP ]; then
-  echo "❌ ERROR: Set the DEPLOY_CONSOLE_IP environment variable to the console IP."
-  DEPLOY_CONSOLE_USER="${DEPLOY_CONSOLE_USER:-root}"
-  DEPLOY_GAME_NAME="${DEPLOY_CONSOLE_USER:-PygameVideogameMaker}"
-  DEPLOY_DEST_DIR="${DEPLOY_DEST_DIR:-/userdata/roms/pygame}"
-  DEST="$DEPLOY_CONSOLE_USER@$IP:$DEPLOY_DEST_DIR"
-  GAME_NAME="$DEPLOY_GAME_NAME"
-fi
-
-PROJECT_DIR="."
-PROJECT_DIR_DEST="$DEST/$GAME_NAME"
-
 set -eu
-rm -rf vendor
-mkdir -p vendor
+
+IP="${DEPLOY_CONSOLE_IP:-}"
+[ -z "$IP" ] && echo "❌ Set DEPLOY_CONSOLE_IP" && exit 1
+
+DEPLOY_CONSOLE_USER="${DEPLOY_CONSOLE_USER:-root}"
+DEPLOY_GAME_NAME="${DEPLOY_GAME_NAME:-PygameVideogameMaker}"
+DEPLOY_DEST_DIR="/userdata/roms/pygame"
+
+DEST_HOST="${DEPLOY_CONSOLE_USER}@${IP}"
+REMOTE_BASE="${DEPLOY_DEST_DIR}/${DEPLOY_GAME_NAME}"
+
+echo "📦 Root: ."
+echo "🎯 Dest: ${DEST_HOST}:${REMOTE_BASE}"
+
 
 uv pip freeze \
   | grep -vE '^-e |^file:|^\.$' \
   | sed 's/@.*//' \
   | xargs -n1 uv pip install --target vendor --no-compile
 
-
-echo "✅ vendor/ ready"
-echo "📦 Root: $PROJECT_DIR"
-echo "🎯 Dest: $PROJECT_DIR_DEST"
-
-# 1) Sync the project
+# 2) proyecto
 rsync -av --delete --no-owner --no-group \
   --exclude ".venv/" \
   --exclude "__pycache__/" \
-  "$PROJECT_DIR" \
-  "$PROJECT_DIR_DEST"
+  ./ \
+  "${DEST_HOST}:${REMOTE_BASE}/"
+
 
 echo "✅ Synced."
